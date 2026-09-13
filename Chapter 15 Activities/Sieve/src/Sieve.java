fba8f0ff2f849f16b8fd85f3341dae8ab719e83c
zthoersten <zthoersten@stu.naperville203.org>
2026-09-12 20:04:43 -0500

    complete

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
-
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * A program that implements the sieve of Eratosthenes.
 */
-public class Sieve
-{
-    public static void main(String[] args)
-    {
+public class Sieve {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
-
-        // Your work goes here
-        . . .
-
-
-
-
-
-
-
+        
+        SortedSet<Integer> primes = new TreeSet<>();
+        for (int i = 2; i < n; i++) {
+            primes.add(i);
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primes.remove(i);
+                    break;
+                }
+            }
+        }
+        
+        System.out.println(primes);
     }
 }
